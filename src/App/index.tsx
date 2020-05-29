2a17570d5b00f2b2e8f36cf0416c89b49594a76d
Chance Graff <cgraffrun@gmail.com>
2020-05-29 17:16:43 -0500

    Add typescript, sass

---
+++ src/App/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <span>Hello world!</span>
+    </div>
+  );
+}
+
+export default App;
